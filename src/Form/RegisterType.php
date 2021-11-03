6fe557f1d004416562199fb83b101098eb9fcc4e
Mehdimosteghanemi <mehdi.mosteghanemi@gmail.com>
2021-11-03 11:09:40 +0100

    formatting form

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +15,30 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email')
-            ->add('password')
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prénom',
+                'attr' => [
+                    'placeholder' => 'exemple Laurent'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'exemple Delaporte'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre e-mail',
+                'attr' => [
+                    'placeholder' => 'exemple@exemple.fr'
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Votre mot de passe',
+                'attr' => [
+                    'placeholder' => ''
+                ]
+            ])
             // ->add('roles')
         ;
     }
