90fe566c817f48006c74279dca00e24b15f5f436
Mehdimosteghanemi <mehdi.mosteghanemi@gmail.com>
2021-11-10 10:49:32 +0100

    add changepassword controller, add changepassword form & add template

---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'disabled' => true,
+                'label' => 'Mon adresse e-mail'
+            ])
+            ->add('firstname', TextType::class, [
+                'disabled' => true,
+                'label' => 'Mon prénom'
+            ])
+            ->add('lastname', TextType::class, [
+                'disabled' => true,
+                'label' => 'Mon nom'
+            ])
+            ->add('password')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
