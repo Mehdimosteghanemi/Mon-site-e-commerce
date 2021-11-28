e55cc4ac35389d4dd04ed35de11845bcdfe298b7
Mehdimosteghanemi <mehdi.mosteghanemi@gmail.com>
2021-11-28 12:24:21 +0100

    mapping entity easyadmin wip

---
+++ src/Controller/Admin/OrderCrudController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Order;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+
+class OrderCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Order::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+
+        return [
+            IdField::new('id'),
+            DateField::new('createdAt', 'Passée le')->setFormat('dd-MM-yyyy hh:mm:ss'),
+        ];
+    }
+    
+}
